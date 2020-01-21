ab80f5b2bd246b64c42aaa0dc756344801217b19
Bia Vieira <biavieira@Bias-MacBook-Air.local>
2020-01-21 20:22:34 -0300

    Structure Layout

---
+++ src/components/Products/ListProducts/ListProducts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListProducts extends Component {
+    render() {
+        return (
+            <div>
+                List Products
+            </div>
+        )
+    }
+}
+
+export default ListProducts;
