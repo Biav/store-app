5b8f6c9dc2ee0ffaae32c8ee96959c6a6d3eb3ce
Bia Vieira <biavieira@Bias-MacBook-Air.local>
2020-01-26 15:13:35 -0300

    Remove icons Footer

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -11,14 +11,6 @@ const footer = () => (
             <script>document.write(new Date().getFullYear());</script>
             2020 All rights reserved | This template is made with <i className="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_new">Colorlib</a>
           </div>
-          <div className="col-lg-4 col-md-12 footer-social">
-            <a href="https://www.facebook.com/" className="icons facebook">
-              <span></span>
-            </a>
-            <a href="https://www.instagram.com/?hl=pt-br" className="icons instagram">
-              <span></span>
-            </a>
-          </div>
         </div>
       </div>
     </footer>
