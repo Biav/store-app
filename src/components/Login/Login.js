dc185e3a03afe4be6526208f332812cd43f0b41a
Bia <beatrizvieirasilva.v@gmail.com>
2020-02-12 14:06:00 -0300

    Authentication Facebook and Google

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 class Login extends Component {
     render() {
+
+        const responseFacebook = (response) => {
+            console.log(response);
+        }
+    
+        const responseGoogle = (response) => {
+            console.log(response);
+        }
+        
         return(
             <>
-                Login
+                <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
+
+                <FacebookLogin
+                appId="575249743205710" //APP ID NOT CREATED YET
+                fields="name,email,picture"
+                callback={responseFacebook}
+                />
+                <br />
+                <br />
+
+
+                <GoogleLogin
+                clientId="100357852984-14togn75hmr163rck8tk9g06qfp5go29.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
+                buttonText="LOGIN WITH GOOGLE"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                />
             </>
         )
     }
