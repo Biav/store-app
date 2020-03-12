9ae7a88cfaf1020d86a992a39705a1311a9bd0f2
Bia <beatrizvieirasilva.v@gmail.com>
2020-03-12 16:14:57 -0300

    Validation Form Products

--- src/components/Products/AddProduct/Product/ProductForm.js
+++ src/components/Products/AddProduct/Product/ProductForm.js
@@ -1,4 +1,5 @@
 import React, { Component} from 'react';
+import SimpleReactValidator from 'simple-react-validator';
 
 class ProductForm extends Component {
 
@@ -12,12 +13,24 @@ class ProductForm extends Component {
 				productCategory: '',
 				productDescription: ''
 		}
+
+		this.validator = new SimpleReactValidator({autoForceUpdate: this});
 	}
 
 	handleInput(name, e) {
 		this.setState({
 			[name]: e.target.value
 		})
+	}
+
+	addProduct(event) {
+		event.preventDefault();
+
+		if (this.validator.allValid()) {
+			console.log("test")
+		} else {
+			this.validator.showMessages();
+		}
 	}
 
 	render() {
@@ -28,23 +41,44 @@ class ProductForm extends Component {
 						<div className="form-group">
 							<label for="inputEmail3" className="col-sm-2 control-label">Produto</label>
 							<div className="col-sm-10">
-								<input type="text" className="form-control" name="productName" placeholder="Nome do Produto"
-											 value={this.state.productName}
-											 onChange={(e) => this.handleInput('productName', e)}/>
+								<input type="text" 
+									   className="form-control" 
+									   name="productName" 
+									   placeholder="Nome do Produto"
+									   value={this.state.productName}
+									   onChange={(e) => this.handleInput('productName', e)}
+									   onBlur={() => this.validator.showMessageFor('productName')}/>
+								
+								{this.validator.message('productName', this.state.productName, 'required')}
+
 							</div>
 						</div>
 						<div className="form-inline col-md-10">
 							<div className="form-group">
 								<label for="exampleInputEmail3">Quantidade</label>
-								<input type="number" className="form-control" name="productTotal" placeholder="Quantidade"
+								<input type="number" 
+											 className="form-control" 
+											 name="productTotal" 
+											 placeholder="Quantidade"
 											 value={this.state.productTotal}
-											 onChange={(e) => this.handleInput('productTotal', e)}/>
+											 onChange={(e) => this.handleInput('productTotal', e)}
+											 onBlur={() => this.validator.showMessageFor('productTotal')}/>
+								
+								{this.validator.message('productTotal', this.state.productTotal, 'required|numeric')}
+
 							</div>
 							<div className="form-group">
 								<label for="exampleInputPassword3">Preço</label>
-								<input type="text" className="form-control" name="productPrice" placeholder="Preço"
+								<input type="text" 
+											 className="form-control" 
+											 name="productPrice" 
+											 placeholder="Preço"
 											 value={this.state.productPrice}
-											 onChange={(e) => this.handleInput('productPrice', e)}/>
+											 onChange={(e) => this.handleInput('productPrice', e)}
+											 onBlur={() => this.validator.showMessageFor('productPrice')}/>
+								
+								{this.validator.message('productPrice', this.state.productPrice, 'required')}
+
 							</div>
 							<div className="form-group">
 								<label for="exampleInputPassword3">Categoria</label>
@@ -58,14 +92,26 @@ class ProductForm extends Component {
 						<div className="form-group">
 							<label for="inputPassword3" className="col-sm-2 control-label">Descrição</label>
 							<div className="col-sm-10">
-								<input type="textarea" className="form-control" id="description" placeholder="Descrição do Produto"
+								<textarea type="textarea" 
+											 className="form-control" 
+											 id="description" 
+											 rows="3"
+											 placeholder="Descrição do Produto"
 											 value={this.state.productDescription}
-											 onChange={(e) => this.handleInput('productDescription', e)}/>
+											 onChange={(e) => this.handleInput('productDescription', e)}
+											 onBlur={() => this.validator.showMessageFor('productDescription')}/>
+
+									{this.validator.message('productDescription', this.state.productDescription, 'required')}
+
 							</div>
 						</div>
 						<div className="form-group">
 							<div className="col-sm-offset-2 col-sm-10">
-									<button type="submit" className="btn btn-primary" >Salvar</button>
+								<button type="submit" 
+										className="btn btn-primary" 
+										onClick={(e) => this.addProduct(e)}>
+									Salvar
+								</button>
 							</div>
 						</div>
 					</form>
